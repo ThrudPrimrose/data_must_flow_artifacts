7a9d923bdfa80a08ff72ccf684513453868e2776
Yakup Budanaz <ybudanaz@ethz.ch>
2025-11-24 21:45:07 +0100

    Add csr spmv

--- csr_spmv/gcc/amd_epyc/submit.sh
+++ csr_spmv/gcc/amd_epyc/submit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=csc1_amd_gcc  # Job name
+#SBATCH --job-name=csr_amd_gcc  # Job name
 #SBATCH --nodes=1                     # Number of nodes
 #SBATCH --partition=amd               # Partition/queue
 #SBATCH --time=01:00:00               # Walltime (hh:mm:ss)
 #SBATCH --output=%x_%j.out            # Standard output (%x=job name, %j=job ID)
 #SBATCH --error=%x_%j.err             # Standard error
-#SBATCH --chdir=/scratch/ybudanaz/data_must_flow_artifacts/cloudsc_pattern_one/gcc/amd_epyc
+#SBATCH --chdir=/scratch/ybudanaz/data_must_flow_artifacts/csr_spmv/gcc/amd_epyc
 
 spack load cmake
 spack load gcc@14.2
@@ -16,4 +16,4 @@ alias cxx=g++
 export CC=gcc
 export CXX=g++
 
-python3 benchmark_cloudsc_pattern_one.py
+python3 csr_gemmver.py
--- csr_spmv/llvm/intel_xeon/submit.sh
+++ csr_spmv/llvm/intel_xeon/submit.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=csc1_intel_llvm  # Job name
+#SBATCH --job-name=csr_intel_llvm  # Job name
 #SBATCH --nodes=1                     # Number of nodes
 #SBATCH --partition=intel               # Partition/queue
 #SBATCH --time=01:00:00               # Walltime (hh:mm:ss)
 #SBATCH --output=%x_%j.out            # Standard output (%x=job name, %j=job ID)
 #SBATCH --error=%x_%j.err             # Standard error
-#SBATCH --chdir=/scratch/ybudanaz/data_must_flow_artifacts/cloudsc_pattern_one/llvm/intel_xeon
+#SBATCH --chdir=/scratch/ybudanaz/data_must_flow_artifacts/csr_spmv/llvm/intel_xeon
 
 spack load cmake
 spack load gcc@14.2
@@ -16,4 +16,4 @@ alias cxx=/scratch/ybudanaz/bin/clang++
 export CC=/scratch/ybudanaz/bin/clang
 export CXX=/scratch/ybudanaz/bin/clang++
 
-python3 benchmark_cloudsc_pattern_one.py
+python3 csr_gemmver.py
