ef8907021fb86f114f3a04d3b579fb17375ab579
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2025-11-27 18:22:06 +0100

    Update template

--- template/gcc/arm/submit_daint.sh
+++ template/gcc/arm/submit_daint.sh
@@ -20,6 +20,9 @@ export CXX=g++
 # Define configurations: each element is "EXTRA_FLAGS SUFFIX"
 configs=(
     "" ""                                   # first run: no extra flags, no suffix
+    "-mcpu=neoverse-v2 -msve-vector-bits=512 -mllvm -enable-scalable-autovec-in-streaming-mode" "sve_512"
+    "-mcpu=neoverse-v2 -msve-vector-bits=128 -mllvm -enable-scalable-autovec-in-streaming-mode" "sve_128"
+    "-fvectorize" "neon"
 )
 
 for ((i=0; i<${#configs[@]}; i+=2)); do
