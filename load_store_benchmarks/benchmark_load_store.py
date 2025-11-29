97a7ffc55e3af268b9c678cae355ae2fefa8204b
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2025-11-29 16:07:37 +0100

    New size and time lims

--- load_store_benchmarks/benchmark_load_store.py
+++ load_store_benchmarks/benchmark_load_store.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
     all_timings = {}
 
     # S = dace.symbol("S")
-    for i, N in enumerate([8192,]):
+    for i, S in enumerate([8192 * 64, 8192 * 256, 8192 * 512, 8192 * 1024, 8192 * 2048, 8192 * 4096, 8192 * 8192, 8192 * 8192 * 2]):
         @dace.program
         def vecscale_unit_stride(src: dace.float64[N], dst: dace.float64[N], scale: dace.float64):
             for i, in dace.map[0:N:1]:
--- load_store_benchmarks/llvm/amd_epyc/submit.sh
+++ load_store_benchmarks/llvm/amd_epyc/submit.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=log_amd_epyc_llvm  # Job name
 #SBATCH --nodes=1                     # Number of nodes
 #SBATCH --partition=amd               # Partition/queue
-#SBATCH --time=00:30:00               # Walltime (hh:mm:ss)
+#SBATCH --time=02:00:00               # Walltime (hh:mm:ss)
 #SBATCH --output=%x_%j.out            # Standard output (%x=job name, %j=job ID)
 #SBATCH --error=%x_%j.err             # Standard error
 #SBATCH --chdir=.
