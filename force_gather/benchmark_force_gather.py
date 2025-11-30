a806cd4462032a1b000194a38b105147820d21dc
Yakup Budanaz <ybudanaz@ethz.ch>
2025-11-30 17:23:33 +0100

    Force gather benchmark using intrinsics

---
+++ force_gather/benchmark_force_gather.py
@@ -0,0 +1,214 @@
+import copy
+import csv
+import os
+import numpy as np
+import dace
+from dace.transformation.passes.vectorization.vectorize_cpu import VectorizeCPU
+from dace.transformation.passes.vectorization.detect_gather import DetectGather
+from math import log
+
+import subprocess
+
+# Base compilation flags
+base_flags = [
+    '-fopenmp', '-fstrict-aliasing', '-std=c++17', '-faligned-new',
+    '-fPIC', '-Wall', '-Wextra', '-O3', '-march=native', '-ffast-math',
+    '-Wno-unused-parameter', '-Wno-unused-label'
+]
+
+# Architecture / compiler specific extra flags
+env_flags_str = os.environ.get('EXTRA_FLAGS', '')
+base_flags_str = ' '.join(base_flags)
+
+flags = base_flags_str + " " + env_flags_str if env_flags_str != '' else base_flags_str
+dace.config.Config.set("compiler", "cpu", "args", value=flags)
+
+compiler_exec = os.environ.get('CXX', 'c++')
+dace.config.Config.set("compiler", "cpu", "executable", value=compiler_exec)
+
+env_suffix_str = os.environ.get('SUFFIX', '')
+if env_suffix_str != '':
+    env_suffix_str = "_" + env_suffix_str
+
+def get_physical_cores():
+    # Use lscpu and parse "Core(s) per socket" and "Socket(s)"
+    lscpu_dst = subprocess.check_output(["lscpu"]).decode()
+    cores_per_socket = None
+    sockets = None
+    for line in lscpu_dst.splitlines():
+        if "Core(s) per socket:" in line:
+            cores_per_socket = int(line.split(":")[1])
+        elif "Socket(s):" in line:
+            sockets = int(line.split(":")[1])
+    if cores_per_socket and sockets:
+        return cores_per_socket * sockets
+    # fallback
+    return int(subprocess.check_output(["nproc", "--all"]).decode())
+
+ncores = get_physical_cores()
+print("Physical cores:", ncores)
+
+def init_openmp():
+    # Get physical core count
+    ncores = get_physical_cores()
+
+    # Set OpenMP environment
+    os.environ["OMP_NUM_THREADS"] = "1"
+    os.environ["OMP_PLACES"] = "cores"
+    os.environ["OMP_PROC_BIND"] = "true"
+
+# Call before loading OpenMP-linked C++ libs
+init_openmp()
+
+
+def run_sdfg_multiple_times(
+    sdfg: dace.SDFG,
+    arrays,
+    params,
+    num_runs=10
+):
+    """
+    Runs an SDFG several times and returns the list of execution times
+    recorded by DaCe instrumentation.
+
+    Returns:
+        List[float] – execution time (in seconds) for each run.
+    """
+
+    sdfg.instrument = dace.dtypes.InstrumentationType.Timer
+
+
+    compiled = sdfg.compile()
+    compiled(**arrays, **params)  # Warm-up run
+
+    times = []
+
+    for it in range(num_runs):
+        # Deep-copy input arrays for each run
+        # Execute
+        compiled(**arrays, **params)
+
+        # Read last instrumentation entry
+        report = sdfg.get_latest_report()
+        # Or: sdfg.get_instrumentation_reports()[-1]
+        #print(report)
+
+        total_time = report.events[0].duration  # seconds
+        #print(total_time)
+        times.append(total_time)
+        print(f"Run time {sdfg.name} iter {it}: {float(total_time)/1000.0:.3f} ms")
+
+    return times
+
+def save_timings_to_csv(filename, i, isize, timings_dict):
+    """
+    Write:
+        sdfg_name,rep_idx,time_in_seconds
+    """
+    with open(filename, "w" if i == 0 else "a", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["sdfg_name", "size", "rep", "time_seconds"])
+
+        for (sdfg_name, size), times in timings_dict.items():
+            if isize != size:
+                continue
+            for i, t in enumerate(times):
+                writer.writerow([sdfg_name, size, i, t])
+
+# -------------------------------------------------------
+# Helpers
+# -------------------------------------------------------
+
+def build_vectorized_sdfg(base_sdfg, vec_width, insert_copies, suffix, base_name):
+    sdfg = copy.deepcopy(base_sdfg)
+    VectorizeCPU(vector_width=vec_width, insert_copies=insert_copies).apply_pass(sdfg, {})
+    DetectGather().apply_pass(sdfg, {})
+    name = f"{base_name}_static_veclen_{vec_width}_{suffix}"
+    sdfg.name = name
+    sdfg.instrument = dace.dtypes.InstrumentationType.Timer
+    return sdfg, sdfg.name
+
+
+# -------------------------------------------------------
+# Main
+# -------------------------------------------------------
+
+if __name__ == "__main__":
+    NUM_REPS = 20
+    all_timings = {}
+
+    for i, N in enumerate([8192 * 64, 8192 * 256, 8192 * 512, 8192 * 1024, 8192 * 2048, 8192 * 4096, 8192 * 8192, 8192 * 8192 * 2]):
+        @dace.program
+        def gather_load(src: dace.float64[N], idx: dace.int64[N], dst: dace.float64[N], scale: dace.float64):
+            for i, in dace.map[0:N:1]:
+                dst[i] = src[idx[i]] * scale
+
+
+
+        all_funcs = [ # (function, kind, stride)
+            (gather_load, "load", "gather"),
+            ]
+
+        sdfg_list = [(f.to_sdfg(), n, s) for f, n, s in all_funcs]
+
+        # Baseline SDFG
+        for sdfg, variant_name, stride in sdfg_list:
+            sdfg.name = sdfg.name + env_suffix_str
+            sdfg.instrument = dace.dtypes.InstrumentationType.Timer
+
+            src_size = N
+            dst_size = N
+            idx_size = N
+            if variant_name == "load":
+                if stride != "gather" and stride != "scatter":
+                    src_size *= stride
+            elif variant_name == "store":
+                if stride != "gather" and stride != "scatter":
+                    dst_size *= stride
+
+
+            # Baseline arrays
+            src = np.random.random((src_size, ))
+            dst = np.random.random((dst_size, ))
+            base_arrays = {"src": src, "dst": dst, }
+            scale = np.float64(np.random.rand())
+            params = {"scale": scale}
+            if stride == "gather" or stride == "scatter":
+                idx = np.random.permutation(N).astype(np.int64)
+                base_arrays["idx"] = idx
+
+            # Run baseline 1
+            all_timings[sdfg.name, N] = run_sdfg_multiple_times(
+                sdfg=sdfg,
+                arrays=base_arrays,
+                params=params,
+                num_runs=NUM_REPS,
+            )
+
+            # -------------------------------------------------------
+            # Vectorized versions
+            # -------------------------------------------------------
+            for l in [8, 16, 32, 64]:
+                # std no-copy version
+                sdfg_vec, name = build_vectorized_sdfg(
+                    sdfg, vec_width=l, insert_copies=False, suffix=f"no_cpy",
+                    base_name=sdfg.name
+                )
+                all_timings[name, N] = run_sdfg_multiple_times(
+                    sdfg=sdfg_vec, arrays=base_arrays, params=params, num_runs=NUM_REPS
+                )
+
+                # std copy version
+                sdfg_vec_cpy, name = build_vectorized_sdfg(
+                    sdfg, vec_width=l, insert_copies=True, suffix=f"cpy",
+                    base_name=sdfg.name
+                )
+                all_timings[name, N] = run_sdfg_multiple_times(
+                    sdfg=sdfg_vec_cpy, arrays=base_arrays, params=params, num_runs=NUM_REPS
+                )
+
+            # -------------------------------------------------------
+            # CSV dstput
+            # -------------------------------------------------------
+            save_timings_to_csv(f"load_store_benchmarks_timings_1_core{env_suffix_str}.csv", i, N, all_timings)
+            print(f"Saved timing results to load_store_benchmarks_timings_1_core{env_suffix_str}.csv")
---
+++ force_gather/submit_daint.sh
@@ -0,0 +1,6 @@
+cd gcc/arm
+sbatch submit_daint.sh
+cd ../..
+cd llvm/arm
+sbatch submit_daint.sh
+cd ../..
