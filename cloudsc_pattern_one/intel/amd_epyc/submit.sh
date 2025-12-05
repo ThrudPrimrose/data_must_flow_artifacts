8a77761579659d9acb3b1ebb46e495eb5e9343a3
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2025-12-05 13:26:35 +0100

    Add missing script

---
+++ cloudsc_pattern_one/intel/amd_epyc/submit.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --job-name=clsc1_amd_epyc_intel  # Job name
+#SBATCH --nodes=1                     # Number of nodes
+#SBATCH --partition=intel               # Partition/queue
+#SBATCH --time=04:00:00               # Walltime (hh:mm:ss)
+#SBATCH --output=%x_%j.out            # Standard output (%x=job name, %j=job ID)
+#SBATCH --error=%x_%j.err             # Standard error
+#SBATCH --chdir=.
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=72
+
+spack load cmake
+spack load intel-oneapi-compilers@2025.0.4
+
+alias cc=icx
+alias c++=icpx
+alias cxx=icpx
+export CC=icx
+export CXX=icpx
+
+export CPU_NAME="intel_xeon"
+export OMP_NUM_THREADS=36
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+echo "Script path: $SCRIPT_PATH"
+echo "Script dir:  $SCRIPT_DIR"
+
+# Define configurations: each element is "EXTRA_FLAGS SUFFIX"
+configs=(
+    "" ""                                   # first run: no extra flags, no suffix
+    "-qopt-zmm-usage=high -diag-enable=vec -qopt-report=5 -qopt-report-phase=vec" "force_width_512"
+    "-no-vec" "no_vectorize"
+)
+
+for RUNMULTI in 0 1; do
+    export RUN_MULTICORE="$RUNMULTI"
+    for ((i=0; i<${#configs[@]}; i+=2)); do
+        export EXTRA_FLAGS="${configs[i]}"
+        export SUFFIX="${configs[i+1]}"
+
+        echo "Running with EXTRA_FLAGS='$EXTRA_FLAGS', SUFFIX='$SUFFIX'"
+
+        # Copy benchmark script
+        cp ../../benchmark_cloudsc_pattern_one.py .
+
+        # Run benchmark
+        python3 benchmark_cloudsc_pattern_one.py
+
+        # Remove script
+        rm benchmark_cloudsc_pattern_one.py
+    done
+done
