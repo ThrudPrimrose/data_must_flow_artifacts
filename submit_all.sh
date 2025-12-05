56bc9534806215f72b62a62f5fd6a2218717b845
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2025-12-05 13:22:43 +0100

    Submit all

---
+++ submit_all.sh
@@ -0,0 +1,16 @@
+cd cloudsc_pattern_one
+./submitter_ault.sh
+
+cd ../division_by_zero
+./submitter_ault.sh
+
+cd ../force_gather
+./submitter_ault.sh
+
+cd ../log_implementations
+./submitter_ault.sh
+
+cd ../exp_implementations
+./submitter_ault.sh
+
+cd ..
