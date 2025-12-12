0bea06c282e4cfb9ce8ebbff97b983840d6b82b6
Yakup Koray Budanaz <ybudanaz@ethz.ch>
2025-12-13 00:06:42 +0100

    Add runners

---
+++ vector_intrinsics/intel/intel_xeon/submit.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH --job-name=vtrin_intel_xeon_  # Job name
+#SBATCH --nodes=1                     # Number of nodes
+#SBATCH --partition=intel               # Partition/queue
+#SBATCH --time=02:30:00               # Walltime (hh:mm:ss)
+#SBATCH --output=%x_%j.out            # Standard output (%x=job name, %j=job ID)
+#SBATCH --error=%x_%j.err             # Standard error
+#SBATCH --chdir=.
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=72
+
+spack load cmake
+spack load intel-oneapi-compilers@2025.0.4
+
+alias cc=icx
+alias c++=icpx
+alias cxx=icpx
+export CC=icx
+export CXX=icpx
+
+export CPU_NAME="intel_xeon"
+
+echo "Script path: $SCRIPT_PATH"
+echo "Script dir:  $SCRIPT_DIR"
+export OMP_NUM_THREADS=36
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+
+# Define configurations: each element is "EXTRA_FLAGS SUFFIX"
+configs=(
+    "" ""                                   # first run: no extra flags, no suffix
+    "-qopt-zmm-usage=high" "force_width_512"
+    "-no-vec" "no_vectorize"
+)
+
+for RUNMULTI in 0 ; do
+    export RUN_MULTICORE="$RUNMULTI"
+    for ((i=0; i<${#configs[@]}; i+=2)); do
+        export EXTRA_FLAGS="${configs[i]}"
+        export SUFFIX="${configs[i+1]}"
+        export __DACE_N=16384
+        echo "Running with EXTRA_FLAGS='$EXTRA_FLAGS', SUFFIX='$SUFFIX'"
+
+        # Copy benchmark script
+        cp ../../benchmark_vector_intrinsics.py .
+
+        # Run benchmark
+        pytest  -n 1 benchmark_vector_intrinsics.py
+
+        # Remove script
+        rm benchmark_vector_intrinsics.py
+    done
+done
---
+++ vector_intrinsics/llvm/intel_xeon/submit.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#SBATCH --job-name=vtrin_intel_xeon_llvm  # Job name
+#SBATCH --nodes=1                     # Number of nodes
+#SBATCH --partition=intel               # Partition/queue
+#SBATCH --time=02:30:00               # Walltime (hh:mm:ss)
+#SBATCH --output=%x_%j.out            # Standard output (%x=job name, %j=job ID)
+#SBATCH --error=%x_%j.err             # Standard error
+#SBATCH --chdir=.
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=72
+
+spack load cmake
+
+alias cc=clang
+alias c++=clang++
+alias cxx=clang++
+export CC=clang
+export CXX=clang++
+
+export CPU_NAME="intel_xeon"
+
+echo "Script path: $SCRIPT_PATH"
+echo "Script dir:  $SCRIPT_DIR"
+export OMP_NUM_THREADS=36
+export OMP_PLACES=cores
+export OMP_PROC_BIND=close
+
+# Define configurations: each element is "EXTRA_FLAGS SUFFIX"
+configs=(
+    "" ""                                   # first run: no extra flags, no suffix
+    "-mprefer-vector-width=512" "force_width_512"   # second run
+    "-fno-vectorize" "no_vectorize"
+)
+
+for RUNMULTI in 0 ; do
+    export RUN_MULTICORE="$RUNMULTI"
+    for ((i=0; i<${#configs[@]}; i+=2)); do
+        export EXTRA_FLAGS="${configs[i]}"
+        export SUFFIX="${configs[i+1]}"
+        export __DACE_N=16384
+
+        echo "Running with EXTRA_FLAGS='$EXTRA_FLAGS', SUFFIX='$SUFFIX'"
+
+        # Copy benchmark script
+        cp ../../benchmark_vector_intrinsics.py .
+
+        # Run benchmark
+        pytest  -n 1 benchmark_vector_intrinsics.py
+
+        # Remove script
+        rm benchmark_vector_intrinsics.py
+    done
+done
